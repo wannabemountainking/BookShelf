b46537449427782689e0d3cd9932149d03cb1d3d
김도윤 <kdy0096@gmail.com>
2026-03-07 02:49:53 +0900

    Initial Commit

---
+++ BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by yoonie on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
